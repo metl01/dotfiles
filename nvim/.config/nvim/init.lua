daa802e65ffab233d86f9d313e964a5c63d3b9e0
metl01 <99753359+metl01@users.noreply.github.com>
2026-01-14 19:16:44 -0500

    add colorizer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -45,6 +45,31 @@ local plugins = {
       indent = { enable = true },
     },
   },
+
+  {
+    "norcalli/nvim-colorizer.lua",
+      config = function()
+        require("colorizer").setup({
+          "css",
+          "javascript",
+        })
+      end,
+  },
+
+  "nvim-mini/mini.indentscope",
+  version = false,
+  config = function()
+    require("mini.indentscope").setup({
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "Disable mini.indentscope on dashboard and utility buffers",
+        -- You can add other utility filetypes here (e.g., 'help', 'NvimTree', 'lazy', 'Trouble')
+        pattern = { "alpha", "dashboard", "neo-tree" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    })
+  end,
 }
 
 local opts = {}
