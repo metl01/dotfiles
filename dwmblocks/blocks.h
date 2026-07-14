6aa4550b07ef5cfb642027b721875b8d319672c6
metl01 <99753359+metl01@users.noreply.github.com>
2026-07-14 00:17:01 -0400

    change path

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"sb-clock",		30,			0},
-	{"",	"sb-weather",		1800,			0},
-	{"",	"sb-volume",		1,			10},
-	{"",	"sb-battery",		30,			0},
+	{"",	"~/.local/bin/sb-clock.sh",		30,			0},
+	{"",	"~/.local/bin/sb-weather.sh",		1800,			0},
+	{"",	"~/.local/bin/sb-volume.sh",		0,			10},
+	{"",	"~/.local/bin/sb-battery.sh",		30,			0},
 };
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
