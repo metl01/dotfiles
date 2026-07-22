0abc4c7a585d4cfabf17ed700cfe1ae0ab11279a
Noah <brewster.noah1@gmail.com>
2026-07-22 12:20:54 -0400

    add microphone mute keybind

--- dwm-files/dwm/config.h
+++ dwm-files/dwm/config.h
@@ -123,6 +123,7 @@ static const Key keys[] = {
   { 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ && pkill -RTMIN+10 dwmblocks") },
   { 0, XF86XK_AudioLowerVolume, spawn, SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- && pkill -RTMIN+10 dwmblocks") },
   { 0, XF86XK_AudioMute, spawn, SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && pkill -RTMIN+10 dwmblocks") },
+  { 0, XF86XK_AudioMicMute, spawn, SHCMD("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle && pkill -RTMIN+10 dwmblocks") },
 };
 
 /* button definitions */
