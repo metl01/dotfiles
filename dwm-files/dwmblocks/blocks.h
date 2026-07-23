2cbe394224658071a58a89d4dd3a812160db1f78
metl01 <99753359+metl01@users.noreply.github.com>
2026-07-23 19:43:39 -0400

    add block for temp script

--- dwm-files/dwmblocks/blocks.h
+++ dwm-files/dwmblocks/blocks.h
@@ -3,6 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"",	"$HOME/.local/bin/sb-clock.sh",		30,			0},
 	{"",	"$HOME/.local/bin/sb-weather.sh",		1800,			0},
+  {"",	"$HOME/.local/bin/sb-temps.sh",		5,			0},
 	{"",	"$HOME/.local/bin/sb-volume.sh",		0,			10},
 	{"",	"$HOME/.local/bin/sb-battery.sh",		30,			0},
 };
