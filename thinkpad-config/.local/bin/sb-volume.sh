4a5fce18b2c1c343266f03cb571cd11d167a008e
Noah <brewster.noah1@gmail.com>
2026-07-23 09:26:28 -0400

    mv volume icon to left

--- thinkpad-config/.local/bin/sb-volume.sh
+++ thinkpad-config/.local/bin/sb-volume.sh
@@ -3,8 +3,8 @@ vol=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
 muted=$(pactl get-sink-mute @DEFAULT_SINK@ | grep -o "yes")
 if [ "$muted" = "yes" ]; then
     icon=$(printf '\uf6a9')
-    echo "$vol $icon"
+    echo "$icon  $vol"
 else
     icon=$(printf '\uf028')
-    echo "$vol $icon "
+    echo "$icon   $vol"
 fi
