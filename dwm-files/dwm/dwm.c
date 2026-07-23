fb4abb320e50a716b84b6ca1c498092fdde47e7c
metl01 <99753359+metl01@users.noreply.github.com>
2026-07-22 21:52:04 -0400

    fix monitor hover focus

--- dwm-files/dwm/dwm.c
+++ dwm-files/dwm/dwm.c
@@ -829,11 +829,9 @@ drawbar(Monitor *m)
 		stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
-		tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px extra right padding */
-		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
-	}
+	drw_setscheme(drw, scheme[SchemeNorm]);
+	tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px extra right padding */
+	drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
 
 	resizebarwin(m);
 	for (c = m->clients; c; c = c->next) {
@@ -1285,7 +1283,7 @@ monocle(Monitor *m)
 void
 motionnotify(XEvent *e)
 {
-	static Monitor *mon = NULL;
+	/*static Monitor *mon = NULL;
 	Monitor *m;
 	XMotionEvent *ev = &e->xmotion;
 
@@ -1296,7 +1294,8 @@ motionnotify(XEvent *e)
 		selmon = m;
 		focus(NULL);
 	}
-	mon = m;
+	mon = m;*/
+  return;
 }
 
 void
