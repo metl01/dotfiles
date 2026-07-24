b844864ac314eea6ffbee25ed54a7448b9daa8d7
Noah <brewster.noah1@gmail.com>
2026-07-24 11:55:37 -0400

    change dmenu size, increase dmenu font size

--- dwm-files/dmenu/dmenu.c
+++ dwm-files/dmenu/dmenu.c
@@ -633,7 +633,7 @@ setup(void)
 	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
 
 	/* calculate menu geometry */
-	bh = drw->fonts->h + 6;
+	bh = drw->fonts->h;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
 #ifdef XINERAMA
