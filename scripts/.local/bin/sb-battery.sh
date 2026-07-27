3beb82a74682c9813cabc43b1218337b14683caa
metl <brewster.noah1@gmail.com>
2026-07-27 17:00:12 -0400

    more icons idk what im doing

--- scripts/.local/bin/sb-battery.sh
+++ scripts/.local/bin/sb-battery.sh
@@ -4,11 +4,30 @@ batpath="/sys/class/power_supply/BAT0"
 cap=$(cat "$batpath/capacity" 2>/dev/null)
 status=$(cat "$batpath/status" 2>/dev/null)
 case "$status" in
-    Charging) icon=$(printf '\U000f0084') ;;
+    Charging)
+        if   [ "$cap" -le 10 ]; then icon=$(printf '\U000f089c')
+        elif [ "$cap" -le 20 ]; then icon=$(printf '\U000f0086')
+        elif [ "$cap" -le 30 ]; then icon=$(printf '\U000f0087')
+        elif [ "$cap" -le 40 ]; then icon=$(printf '\U000f0088')
+        elif [ "$cap" -le 50 ]; then icon=$(printf '\U000f089d')
+        elif [ "$cap" -le 60 ]; then icon=$(printf '\U000f0089')
+        elif [ "$cap" -le 70 ]; then icon=$(printf '\U000f089e')
+        elif [ "$cap" -le 80 ]; then icon=$(printf '\U000f008a')
+        elif [ "$cap" -le 90 ]; then icon=$(printf '\U000f008b')
+        elif [ "$cap" -le 100 ]; then icon=$(printf '\U000f0085')
+        fi
+        ;;
     Discharging)
-        if [ "$cap" -le 15 ]; then icon=$(printf '\U000f0083')
-        elif [ "$cap" -le 50 ]; then icon=$(printf '\U000f007c')
-        else icon=$(printf '\U000f0080')
+        if   [ "$cap" -le 10 ]; then icon=$(printf '\U000f007a')
+        elif [ "$cap" -le 20 ]; then icon=$(printf '\U000f007b')
+        elif [ "$cap" -le 30 ]; then icon=$(printf '\U000f007c')
+        elif [ "$cap" -le 40 ]; then icon=$(printf '\U000f007d')
+        elif [ "$cap" -le 50 ]; then icon=$(printf '\U000f007e')
+        elif [ "$cap" -le 60 ]; then icon=$(printf '\U000f007f')
+        elif [ "$cap" -le 70 ]; then icon=$(printf '\U000f0080')
+        elif [ "$cap" -le 80 ]; then icon=$(printf '\U000f0081')
+        elif [ "$cap" -le 90 ]; then icon=$(printf '\U000f0082')
+        elif [ "$cap" -le 100 ]; then icon=$(printf '\U000f0079')
         fi
         ;;
     Full) icon=$(printf '\U000f0079') ;;
